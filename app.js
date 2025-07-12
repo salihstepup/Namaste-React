2d4db74716afdb0709dfec47f34f8b9929326d5d
MOHAMMED SALIH TS <mohammedsalihts@MOHAMMEDs-MacBook-Air.local>
2025-07-13 03:19:46 +0530

    remaining codes

--- app.js
+++ app.js
@@ -35,33 +35,62 @@
 
 //if having multiple siblings inside it
 
-const heading2 = React.createElement(
-  "div",
-  { id: "parent" }, //u can give any attrbute here
-  [
-    React.createElement(
-      "div",
-      { id: "child" },
-      // React.createElement("h1", {}, "thaliyee") if it is 1 subling
-      [
-        React.createElement("h1", {}, "thaliyee"), //if we have more than1 siblings we need add in an array
-        React.createElement("h2", {}, "thaliyee2"),
-      ]
-    ),
-    ,
-    React.createElement(
-      "div",
-      { id: "child" },
-      // React.createElement("h1", {}, "thaliyee") if it is 1 subling
-      [
-        React.createElement("h1", {}, "thaliyee"), //if we have more than1 siblings we need add in an array
-        React.createElement("h2", {}, "thaliyee2"),
-      ]
-    ),
-  ]
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import { createRoot } from "react-dom/client";
+
+// const heading2 = React.createElement(
+//   "div",
+//   { id: "parent" }, //u can give any attrbute here
+//   [
+//     React.createElement(
+//       "div",
+//       { id: "child" },
+//       // React.createElement("h1", {}, "thaliyee") if it is 1 subling
+//       [
+//         React.createElement("h1", {}, "thaliyee"), //if we have more than1 siblings we need add in an array
+//         React.createElement("h2", {}, "thaliyee2"),
+//       ]
+//     ),
+//     ,
+//     React.createElement(
+//       "div",
+//       { id: "child" },
+//       // React.createElement("h1", {}, "thaliyee") if it is 1 subling
+//       [
+//         React.createElement("h1", {}, "thaliyee"), //if we have more than1 siblings we need add in an array
+//         React.createElement("h2", {}, "thaliyee2"),
+//       ]
+//     ),
+//   ]
+// );
+
+// console.log("heading", heading2); //it will get object not an  tag (imp)
+
+//using jsx wrt react element and also using component here
+
+const heading2 = <h1>Sali</h1>;
+
+const heading1 = <h2>thumbu</h2>;
+
+const Sample = () => (
+  <div>
+    {heading1}
+
+    {heading2}
+  </div>
 );
 
-console.log("heading", heading2); //it will get object not an  tag (imp)
+// const container = document.getElementById("root");
+// const proot = createRoot(container);
+// proot.render(heading2);
+
+//or
 
 const proot = ReactDOM.createRoot(document.getElementById("root"));
-proot.render(heading2);
+proot.render(<Sample />); //use angle tag for component , and for element we can put directly here
+
+//or
+
+// const proot = createRoot(document.getElementById("root"));
+// proot.render(heading2);
